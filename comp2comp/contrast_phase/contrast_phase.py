85bc6aa5ec66ef0615ca1050da62d2bb92fab819
DMcSweeney <donalmcs1@gmail.com>
2023-12-04 10:42:35 +0000

    Turn off nora call

--- comp2comp/contrast_phase/contrast_phase.py
+++ comp2comp/contrast_phase/contrast_phase.py
@@ -85,7 +85,7 @@ class ContrastPhaseDetection(InferenceClass):
                 crop=None,
                 crop_path=crop_path,
                 task_name="total",
-                nora_tag=None,
+                nora_tag="None",
                 preview=False,
                 nr_threads_resampling=1,
                 nr_threads_saving=6,
